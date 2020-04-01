bf6ff0e5c828c17702af1e9ea6d734e286bc62e9
Tostka <tostka@users.noreply.github.com>
2020-04-01 15:18:35 -0500

    verb-logging
    * 9:23 AM 4/1/2020 updated to include workaround for Test-ModuleManifest failure to reload updated psd1s (uses a force loaded copy of current file), now storing the output of the import-module -force passthrugh ; also added Pester tests for: Author, CompanyName, LicenseURI, PowerShellVersion,CopyRight, !RequiredModule,!ExportedFormatFiles,Prefix,comparre missing Exported functions (to determine which unexported), more detailed checking of Exports, and check CBH for Synopsis,Description & 1+ Example (several are remmed by default)

--- Tests/verb-logging.tests.ps1
+++ Tests/verb-logging.tests.ps1
@@ -15,6 +15,7 @@ Copyright   : (c) 2020 Todd Kadrie
 Github      : https://github.com/tostka
 Tags        : Powershell,Pester,Testing,Development
 REVISIONS
+* 9:23 AM 4/1/2020 updated to include workaround for Test-ModuleManifest failure to reload updated psd1s (uses a force loaded copy of current file), now storing the output of the import-module -force passthrugh ; also added Pester tests for: Author, CompanyName, LicenseURI, PowerShellVersion,CopyRight, !RequiredModule,!ExportedFormatFiles,Prefix,comparre missing Exported functions (to determine which unexported), more detailed checking of Exports, and check CBH for Synopsis,Description & 1+ Example (several are remmed by default)
 * 7:49 AM 3/4/2020 version debugged to work with Psv5.1/ISE/VSC
 .DESCRIPTION
 verb-logging.ps1 - verb-logging Pester Tests
@@ -35,7 +36,18 @@ https://github.com/tostka
 PARAM() ;
 $Verbose = ($VerbosePreference -eq 'Continue') ;
 
-# patch in ISE/VSC etc auto-vari support
+<#
+# patch in ISE support
+if ($psISE){
+    $ScriptDir = Split-Path -Path $psISE.CurrentFile.FullPath ;
+    $ScriptBaseName = split-path -leaf $psise.currentfile.fullpath ;
+    $ScriptNameNoExt = [system.io.path]::GetFilenameWithoutExtension($psise.currentfile.fullpath) ;
+    $PSScriptRoot = $ScriptDir ;
+    if($PSScriptRoot -ne $ScriptDir){ write-warning "UNABLE TO UPDATE BLANK `$PSScriptRoot TO CURRENT `$ScriptDir!"} ;
+    $PSCommandPath = $psise.currentfile.fullpath ;
+    if($PSCommandPath -ne $psise.currentfile.fullpath){ write-warning "UNABLE TO UPDATE BLANK `$PSCommandPath TO CURRENT `$psise.currentfile.fullpath!"} ;
+}
+#>
 if ($PSScriptRoot -eq "") {
     if ($psISE){
         $ScriptName = $psISE.CurrentFile.FullPath ; 
@@ -71,6 +83,8 @@ if ($PSScriptRoot -eq "") {
 
 $ModuleName = Split-Path (Resolve-Path "$ScriptDir\..\" ) -Leaf ; 
 $ModuleManifest = (Resolve-Path "$ScriptDir\..\$ModuleName\$ModuleName.psd1").path ; 
+# work around 'never-reloads' bug in Test-ModuleManifest, by force-loading a fresh hash of *curr* manifest, for use in all but *initial* TMM tests
+$Script:ManifestHash = Invoke-Expression (Get-Content $ModuleManifest -Raw)
 $ProjectRoot = (Resolve-path "$ScriptDir\..\").path ; 
 if(!(test-path $ProjectRoot\README.md)){
     throw "Unable to resolve ProjectRoot!" ;
@@ -106,8 +120,10 @@ $smsg=@"
 "@ ;
 write-verbose -verbose:$verbose $smsg ;
 
+# Force Import the module and store the information about the module
 Get-Module $ModuleName | Remove-Module
-Import-Module $ModuleManifest -Force
+$ModuleInformation = Import-Module $ModuleManifest -Force -PassThru ; 
+
 
 Describe 'Module Information' -Tags 'Command'{
     Context 'Manifest Testing' {
@@ -116,21 +132,62 @@ Describe 'Module Information' -Tags 'Command'{
                 $Script:Manifest = Test-ModuleManifest -Path $ModuleManifest -ErrorAction Stop -WarningAction SilentlyContinue
             } | Should Not Throw
         }
+        # Name & Version are not values in psd1 - they're *asserted* by TMM, don't use the hash for these tests
         It 'Valid Manifest Name' {
             $Script:Manifest.Name | Should -Be $ModuleName
         }
         It 'Generic Version Check' {
             $Script:Manifest.Version -as [Version] | Should -Not -BeNullOrEmpty
         }
-        It 'Valid Manifest Description' {
-            $Script:Manifest.Description | Should -Not -BeNullOrEmpty
+        <#
+        It 'Valid PowerShellVersion value' {
+            $Script:ManifestHash.PowerShellVersion | Should Not BeNullOrEmpty
         }
+        #>
+        It "Valid Author"{
+            $Script:ManifestHash.Author | Should not BeNullOrEmpty
+        }
+        It "Valid Company Name"{
+            $Script:ManifestHash.CompanyName | Should not BeNullOrEmpty
+        }
+        It "Valid License"{
+            $ModuleInformation.LicenseURI | Should not BeNullOrEmpty
+        }
+        <#
+        It "has a valid copyright" {
+          $Script:ManifestHash.CopyRight | Should Not BeNullOrEmpty
+        }
+        #>
+        It 'Valid Manifest Description' {
+            $Script:ManifestHash.Description | Should -Not -BeNullOrEmpty
+        }
+        <# via New-ModuleManifest -Tags 
+        It "Valid Tags"{
+            @($Script:ManifestHash.PrivateData.PSData.Tags).Count -gt 0 | Should Be $true
+        }
+        #>
         It 'Valid Manifest Root Module' {
-            $Script:Manifest.RootModule | Should -Be "$ModuleName.psm1"
+            $Script:ManifestHash.RootModule | Should -Be "$ModuleName.psm1"
         }
         It 'Valid Manifest GUID' {
-            $Script:Manifest.Guid | Should -Be "b9637e55-12be-4916-8000-a949f9426fa3"
+            $Script:ManifestHash.Guid | Should -Be "b9637e55-12be-4916-8000-a949f9426fa3"
         }
+        <#
+        It 'No Format File' {
+            $Script:ManifestHash.ExportedFormatFiles | Should BeNullOrEmpty
+        }
+        #>
+        # added from https://powershell.getchell.org/2016/05/16/generic-pester-tests/
+        It 'Required Modules' {
+            $Script:ManifestHash.RequiredModules | Should BeNullOrEmpty
+        }
+        
+        <# extra mani tests:
+        # prefix is the prop if fed from TMM, DefaultPrefix is the raw hash key, both are defaulted commandprefixes
+        It "has a valid prefix" {
+          $Script:ManifestHash.DefaultCommandPrefix | Should Not BeNullOrEmpty
+        }
+        #>
     }
 
     Context 'Exported Functions' {
@@ -152,12 +209,53 @@ Describe 'Module Information' -Tags 'Command'{
 
 "@ ;
             write-verbose -verbose:$verbose $smsg ;
+            
 
             $ExportedCount | Should be $FileCount
         }
+        
+        # another approach
+        It "Exports All Public Functions" {
+            $ExFunctions = $Script:ManifestHash.FunctionsToExport
+            $FunctionFiles = Get-ChildItem -Path "$ProjectRoot\Public" -Filter *.ps1 -Recurse |
+                Select-Object -ExpandProperty BaseName
+            $FunctionNames = $FunctionFiles
+            foreach ($FunctionName in $FunctionNames){
+                $ExFunctions -contains $FunctionName | Should Be $true
+            }
+        }
+        # https://lazywinadmin.com/2016/05/using-pester-to-test-your-manifest-file.html
+        It "Compare Missing Functions"{
+            if (-not ($ExportedCount -eq $FileCount))
+            {
+                $Compare = Compare-Object -ReferenceObject $ExFunctions -DifferenceObject $FunctionFiles
+                $Compare.inputobject -join ',' | Should BeNullOrEmpty
+            }
+        }
+        
     }
 
 
+}
+
+<# Help Tests, functions should have Synopsis,Description & an Example
+https://powershell.getchell.org/2016/05/16/generic-pester-tests/
+#>
+Get-Command -Module $ModuleName | ForEach-Object {
+    Describe 'Help' -Tags 'Help' {
+        Context "Function - $_" { 
+            It 'Synopsis' {
+                Get-Help $_ | Select-Object -ExpandProperty synopsis | should not benullorempty
+            }
+            It 'Description' {
+                Get-Help $_ | Select-Object -ExpandProperty Description | should not benullorempty
+            }
+            It 'Examples' {
+                $Examples = Get-Help $_ | Select-Object -ExpandProperty Examples | Measure-Object 
+                $Examples.Count -gt 0 | Should be $true
+            }
+        }
+    }
 }
 
 Describe 'General - Testing all scripts and modules against the Script Analyzer Rules' {
@@ -172,10 +270,31 @@ Describe 'General - Testing all scripts and modules against the Script Analyzer 
 } 
 
 #region PSSA Testing
-
 <# Calling PS Script analyzer with Pester : PowerShell - https://www.reddit.com/r/PowerShell/comments/6tmprp/calling_ps_script_analyzer_with_pester/
 Shepherd_Ra, 2018
-above extended with updated rule skip logic - shows *all* rules, marks skips
+#>
+<#
+Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
+    Context 'PSSA Standard Rules' {
+        $ScriptAnalyzerSettings = Get-Content -Path $scriptStylePath | Out-String | Invoke-Expression
+        #$AnalyzerIssues = Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\MyScript.ps1" -Settings "$PSScriptRoot\..\ScriptAnalyzerSettings.psd1"
+        $AnalyzerIssues = Invoke-ScriptAnalyzer -Path $ProjectRoot -Recurse -Settings $scriptStylePath
+        $ScriptAnalyzerRuleNames = Get-ScriptAnalyzerRule | Select-Object -ExpandProperty RuleName
+        forEach ($Rule in $ScriptAnalyzerRuleNames) {
+            $Skip = @{Skip=$False}
+            if ($ScriptAnalyzerSettings.ExcludeRules -notcontains $Rule) {
+                # We still want it in the tests, but since it doesn't actually get tested we will skip
+                $Skip = @{Skip = $True}
+            }
+            It "Should pass $Rule" @Skip {
+                $Failures = $AnalyzerIssues | Where-Object -Property RuleName -EQ -Value $rule
+                ($Failures | Measure-Object).Count | Should Be 0
+            }
+        }
+    }
+}
+#>
+<# above extended with updated rule skip logic - shows *all* rules, marks skips
 #>
 Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
     Context 'PSSA Standard Rules' {
@@ -202,57 +321,72 @@ Describe -Tags 'PSSA' -Name 'Testing against PSScriptAnalyzer rules' {
     }
 }
 
-
-<# report profiling
-$report = import-clixml -path $path ; 
-write-verbose -verbose:$verbose "`n`$Report | group severity" 
-"$(($report | group severity | sort count | ft -auto count,name|out-string).trim())`n" ;
-
-write-verbose -verbose:$verbose "`n`$Report | group rulename"
-"$(($report | group rulename | sort count | ft -auto count,name|out-string).trim())`n" ;
-
-write-verbose -verbose:$verbose "`n`$Report | group scriptname"
-"$(($report | group scriptpath | sort -desc count | ft -auto count,name|out-string).trim())`n" ;
-
-write-verbose -verbose:$verbose "`n Detailed per script report:" ;
-foreach ($scriptrpt in $scriptreported ) {
-    $sBnrS = "`n#*------v $($scriptrpt): v------" ;
-    "$($sBnrS)" ;
-    "$(($report |?{$_.scriptname -eq $scriptrpt} | sort rulename,message | ft -auto Severity,Line,RuleName,Message|out-string).trim())`n" ;
-    "$($sBnrS.replace('-v','-^').replace('v-','^-'))" ;
-} ;
+<#
+Describe 'Testing against ScriptAnalyzer rules' {
+    Context "Rules:$scriptStylePath" {
+        $report = Invoke-ScriptAnalyzer -Path $ProjectRoot -Recurse -Settings $scriptStylePath
+        #$scriptAnalyzerRules = Get-ScriptAnalyzerRule
+        $rulesHash = Import-PowerShellDataFile -path $scriptStylePath ;
+        #forEach ($rule in $scriptAnalyzerRules) {
+        forEach ($rule in $ruleshash.IncludeRules) {        
+            It "Should pass $rule" {
+                If ($report.RuleName -contains $rule) {
+                    $report |
+                    Where RuleName -EQ $rule -outvariable failures |
+                    Out-Default
+                    $failures.Count | Should Be 0
+                }
+            }
+        }
+    }
+}
 #>
 
+<#
+Describe 'General - Testing all scripts and modules against the Script Analyzer Rules' {
+    Context "Checking files to test exist and Invoke-ScriptAnalyzer cmdLet is available" {
+        It "Checking files exist to test." {
+            $moduleComponents.count | Should Not Be 0
+        }
+        It "Checking Invoke-ScriptAnalyzer exists." {
+            { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should Not Throw
+        }
+    }
+
+    Context "Checking component files against settings:$scriptStylePath" {
+        #write-verbose -verbose:$verbose "Using Settings:$($scriptStylePath)"
+        
+
+        $Report = Invoke-ScriptAnalyzer -Path $ProjectRoot -Recurse -Settings $scriptStylePath ;
+
+        $Report.count | Should Be 0
+
+        if ($verbose) {
+            write-verbose -verbose:$verbose "`n`$Report | group severity" 
+            "$(($report | group severity | sort count | ft -auto count,name|out-string).trim())`n" ;
+
+            write-verbose -verbose:$verbose "`n`$Report | group rulename"
+            "$(($report | group rulename | sort count | ft -auto count,name|out-string).trim())`n" ;
+
+            write-verbose -verbose:$verbose "`n`$Report | group scriptname"
+            "$(($report | group scriptpath | sort -desc count | ft -auto count,name|out-string).trim())`n" ;
+
+            write-verbose -verbose:$verbose "`n Detailed per script report:" ;
+            foreach ($scriptrpt in $scriptreported ) {
+                $sBnrS = "`n#*------v $($scriptrpt): v------" ;
+                "$($sBnrS)" ;
+                "$(($report |?{$_.scriptname -eq $scriptrpt} | sort rulename,message | ft -auto Severity,Line,RuleName,Message|out-string).trim())`n" ;
+                "$($sBnrS.replace('-v','-^').replace('v-','^-'))" ;
+            } ;
+        };
+    }
+
+    #>
+
+
+
 $ofile = join-path -path (split-path $scriptStylePath) -child "ScriptAnalyzer-Results-$(get-date -format 'yyyyMMdd-HHmmtt').xml" ;
-$Report | export-clixml -path $ofile
+$AnalyzerIssues | export-clixml -path $ofile
 write-verbose -verbose:$verbose  "ScriptAnalyzer Report written to:`n$($ofile)" ;
 
 #endregion
-     
-
-
-# SIG # Begin signature block
-# MIIELgYJKoZIhvcNAQcCoIIEHzCCBBsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUY7KFh2rhjh7DtMmroKvcgpuK
-# xY+gggI4MIICNDCCAaGgAwIBAgIQWsnStFUuSIVNR8uhNSlE6TAJBgUrDgMCHQUA
-# MCwxKjAoBgNVBAMTIVBvd2VyU2hlbGwgTG9jYWwgQ2VydGlmaWNhdGUgUm9vdDAe
-# Fw0xNDEyMjkxNzA3MzNaFw0zOTEyMzEyMzU5NTlaMBUxEzARBgNVBAMTClRvZGRT
-# ZWxmSUkwgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBALqRVt7uNweTkZZ+16QG
-# a+NnFYNRPPa8Bnm071ohGe27jNWKPVUbDfd0OY2sqCBQCEFVb5pqcIECRRnlhN5H
-# +EEJmm2x9AU0uS7IHxHeUo8fkW4vm49adkat5gAoOZOwbuNntBOAJy9LCyNs4F1I
-# KKphP3TyDwe8XqsEVwB2m9FPAgMBAAGjdjB0MBMGA1UdJQQMMAoGCCsGAQUFBwMD
-# MF0GA1UdAQRWMFSAEL95r+Rh65kgqZl+tgchMuKhLjAsMSowKAYDVQQDEyFQb3dl
-# clNoZWxsIExvY2FsIENlcnRpZmljYXRlIFJvb3SCEGwiXbeZNci7Rxiz/r43gVsw
-# CQYFKw4DAh0FAAOBgQB6ECSnXHUs7/bCr6Z556K6IDJNWsccjcV89fHA/zKMX0w0
-# 6NefCtxas/QHUA9mS87HRHLzKjFqweA3BnQ5lr5mPDlho8U90Nvtpj58G9I5SPUg
-# CspNr5jEHOL5EdJFBIv3zI2jQ8TPbFGC0Cz72+4oYzSxWpftNX41MmEsZkMaADGC
-# AWAwggFcAgEBMEAwLDEqMCgGA1UEAxMhUG93ZXJTaGVsbCBMb2NhbCBDZXJ0aWZp
-# Y2F0ZSBSb290AhBaydK0VS5IhU1Hy6E1KUTpMAkGBSsOAwIaBQCgeDAYBgorBgEE
-# AYI3AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwG
-# CisGAQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBRj4Jhw
-# KZhlJlsgPt5n0udU10y3mTANBgkqhkiG9w0BAQEFAASBgDhLvRK+WEXSvG7SyPYk
-# Yiiy5dRxn0Y9TAGEHX/LQ5RLK/VcjCTgdETOOigUPqVOVojeeGn+R4DkyweeOFki
-# Y6/74Ntv5l+BUk3hD2CuiMYLn8HN1GaLmJH9/uYbNv4pruhLT1SYrOz0JN/8L4sH
-# wvNB1DEjlLEczZUCroeoQfh8
-# SIG # End signature block
--- rebuild-module.ps1
+++ rebuild-module.ps1
@@ -41,4 +41,4 @@ PARAM(
     $Verbose = ($VerbosePreference -eq 'Continue') ; 
     write-verbose -verbose:$verbose "`$PSBoundParameters:`n$(($PSBoundParameters|out-string).trim())" ; 
 
-.\process-NewModule.ps1 -ModuleName "verb-logging" -ModDirPath "C:\sc\verb-logging" -Repository "`$localPSRepo" -Merge -showdebug -whatif:$($whatif) ;
+.\process-NewModule.ps1 -ModuleName "verb-logging" -ModDirPath "C:\sc\verb-logging" -Repository "`$localPSRepo" -Merge -RunTest -showdebug -whatif:$($whatif) ;
